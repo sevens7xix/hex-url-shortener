3f4545f8a53da94cc51580dbed594cfda834d76e
Vicko Quizza <ordinaryvicko@gmail.com>
2023-03-20 18:35:51 -0500

    feat: first version up and running

---
+++ app/cmd/main/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/sevens7xix/hex-url-shortener/app/internal/core/services"
+	"github.com/sevens7xix/hex-url-shortener/app/internal/handlers"
+	"github.com/sevens7xix/hex-url-shortener/app/internal/repositories"
+	"github.com/sevens7xix/hex-url-shortener/app/pkg/utilities"
+)
+
+func main() {
+	RedisRepository := repositories.NewRedisRepository()
+	utilities := utilities.ShortenerImplementation{}
+	service := services.NewService(RedisRepository, utilities)
+	handler := handlers.NewHTTPHandler(service)
+
+	router := httprouter.New()
+	router.GET("/api/v1/generate/:original", handler.ShortenURL)
+	router.GET("/api/v1/resolve/:shortURL", handler.ResolveURL)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
